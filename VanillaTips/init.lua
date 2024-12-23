6b0b88160ee7bde675607df47468aa09edb07fe4
wavelet-noise <2902221+wavelet-noise@users.noreply.github.com>
2024-12-23 01:42:44 +0100

    back to 020

--- VanillaTips/init.lua
+++ VanillaTips/init.lua
@@ -1,54 +1,72 @@
 local VanillaTipsMod = {}
 
 function VanillaTipsMod.init()
+    local i = AutosizeInventory.new_simple()
+    i:add(StaticItem.find("CopperOre"), 1)
+    i:add(StaticItem.find("BasicPlatform"), 1)
+    i:add(StaticItem.find("StoneSmelter"), 1)
+    i:add(StaticItem.find("StoneFurnace"), 1)
+    i:add(StaticItem.find("CopperStirlingEngine"), 1)
+    i:add(StaticItem.find("CopperCompactGenerator"), 1)
+    i:add(StaticItem.find("CopperComputer"), 1)
     db:from_table({
         class = "StaticTip",
         name = "FirstSteps",
-        label = {"FirstSteps", "tips"},
-        description = {"FirstStepsDescription", "tips"},
+        label = Loc.new("FirstSteps", "tips"),
+        description = Loc.new("FirstStepsDescription", "tips"),
         image = "Textures/FirstSteps.png",
-        context = {items = {"CopperOre", "BasicPlatform", "StoneSmelter", "StoneFurnace", "CopperStirlingEngine", "CopperCompactGenerator", "CopperComputer"}}
+        context = i
     })
 
+    local i = AutosizeInventory.new_simple()
+    i:add(StaticItem.find("CopperStirlingEngine"), 1)
+    i:add(StaticItem.find("CopperConnector"), 10)
+    i:add(StaticItem.find("CopperHeatPipe"), 1)
+    i:add(StaticItem.find("SteelFlywheel"), 1)
+    i:add(StaticItem.find("Electricity"), 1)
+    i:add(StaticItem.find("Kinetic"), 1)
+    i:add(StaticItem.find("Heat"), 1)
     db:from_table({
         class = "StaticTip",
         name = "Accessors1",
-        label = {"Accessors1", "tips"},
-        description = {"Accessors1Description", "tips"},
+        label = Loc.new("Accessors1", "tips"),
+        description = Loc.new("Accessors1Description", "tips"),
         image = "Textures/Accessors1.png",
-        context = {items = {"CopperStirlingEngine", "CopperConnector", "CopperHeatPipe", "SteelFlywheel", "Electricity", "Kinetic", "Heat"}}
+        context = i
     })
 
+    local i = AutosizeInventory.new_simple()
+    i:add(StaticItem.find("Screwdriver"), 1)
     db:from_table({
         class = "StaticTip",
         name = "Screwdriver",
-        label = {"Screwdriver", "tips"},
-        description = {"ScrewdriverDescription", "tips"},
+        label = Loc.new("Screwdriver", "tips"),
+        description = Loc.new("ScrewdriverDescription", "tips"),
         image = "Textures/Screwdriver.png",
-        context = {items = {"Screwdriver"}}
+        context = i
     })
 
     db:from_table({
         class = "StaticTip",
         name = "RotationWhileBuilding",
-        label = {"RotationWhileBuilding", "tips"},
-        description = {"RotationWhileBuildingDescription", "tips"},
+        label = Loc.new("RotationWhileBuilding", "tips"),
+        description = Loc.new("RotationWhileBuildingDescription", "tips"),
         image = "Textures/Rotation.png"
     })
 
     db:from_table({
         class = "StaticTip",
         name = "BlockPipette",
-        label = {"BlockPipette", "tips"},
-        description = {"BlockPipetteDescription", "tips"},
+        label = Loc.new("BlockPipette", "tips"),
+        description = Loc.new("BlockPipetteDescription", "tips"),
         image = "Textures/Pipette.png"
     })
 
     db:from_table({
         class = "StaticTip",
         name = "StackTransfers",
-        label = {"StackTransfers", "tips"},
-        description = {"StackTransfersDescription", "tips"},
+        label = Loc.new("StackTransfers", "tips"),
+        description = Loc.new("StackTransfersDescription", "tips"),
         image = "Textures/StackTransfers.png"
     })
 end
