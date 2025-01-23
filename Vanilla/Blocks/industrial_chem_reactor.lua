d6eb935adcc5cd3c6e6387d0498a403fc5c29194
wavelet-noise <ishellstrike@gmail.com>
2025-01-23 01:07:33 +0100

    road to 0.20.0

---
+++ Vanilla/Blocks/industrial_chem_reactor.lua
@@ -0,0 +1,35 @@
+require('Blocks/common')
+
+local base_consumption = 100
+
+local logic = function(self)
+    local crafter = AbstractCrafter.cast(self)
+    crafter.recipes = RecipeDictionary.find("IndustrialChemReactorRecipeDictionary")
+    crafter.speed = VanillaSpeedF(crafter)
+        
+    local inv = ResourceInventory.new(crafter, "InputInv")
+    inv.item = StaticItem.find("Electricity")
+    inv.capacity = VanillaConsumptionF(crafter, base_consumption)
+    crafter.energy_input_inventory = inv
+
+    local acc = ResourceAccessor.new(crafter, "Fluid1InputAccessor")
+    acc.side, acc.pos = Vec3i.left, Vec3i.new(-1,1,1)
+    acc.is_input = true
+    acc.channel = "Fluid"
+    acc.cover = StaticCover.find("FluidInput")
+
+    local acc = ResourceAccessor.new(crafter, "Fluid2InputAccessor")
+    acc.side, acc.pos = Vec3i.left, Vec3i.new(0,1,0)
+    acc.is_input = true
+    acc.channel = "Fluid"
+    acc.cover = StaticCover.find("FluidInput")
+
+    local acc = ResourceAccessor.new(crafter, "Input1")
+    acc.side, acc.pos = Vec3i.front, Vec3i.zero
+    acc.inventory = inv
+    acc.is_input = true
+    acc.channel = "Electricity"
+    acc.cover = StaticCover.find("ElectricityInput")
+end
+
+return { logic_init = logic }
