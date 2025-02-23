27c28270fbeb9e444ca48b08b99b8d2103703354
wavelet-noise <2902221+wavelet-noise@users.noreply.github.com>
2025-02-23 04:25:34 +0100

    Ore overhaul wip

--- Vanilla/init.lua
+++ Vanilla/init.lua
@@ -9,7 +9,10 @@ local function dump(o)
    if type(o) == 'table' then
       local s = '('
       for k,v in pairs(o) do
-         s = s .. k ..' = ' .. dump(v) .. ','
+         if type(k) ~= 'userdata' then
+            s = s .. k ..' = ' .. dump(v) .. ','
+         end
+         
       end
       return s .. ')'
    elseif type(o) == 'userdata' then
@@ -26,22 +29,22 @@ end
 function vanilla_mod.init()
    local es = EventSystem.get_instance()
 
-   es:sub(defines.events.on_player_at_sector, function(context) 
-      --local pos = context.pos * cs.sector_size + Vec3i.new(0,0,23)
-      --print("On sector "..tostring(pos)) 
-      --local block = StaticBlock.find("CopperMacerator")
-      --dim:spawn_block_identity(pos, block)
-      local grid_pos = LargeSectors.world_block_to_grid(pos)
-      if not regions:has_region(grid_pos) then
-         local region = regions:get_region(grid_pos)
+   -- es:sub(defines.events.on_player_at_sector, function(context) 
+   --    --local pos = context.pos * cs.sector_size + Vec3i.new(0,0,23)
+   --    --print("On sector "..tostring(pos)) 
+   --    --local block = StaticBlock.find("CopperMacerator")
+   --    --dim:spawn_block_identity(pos, block)
+   --    local grid_pos = LargeSectors.world_block_to_grid(pos)
+   --    if not regions:has_region(grid_pos) then
+   --       local region = regions:get_region(grid_pos)
 
-         local ms = MapStructure.new()
-         ms.structure = ss
-         ms.offset = Vec2i.zero()
+   --       local ms = MapStructure.new()
+   --       ms.structure = ss
+   --       ms.offset = Vec2i.zero()
 
-         region:add_structure(ms)
-      end
-   end)
+   --       region:add_structure(ms)
+   --    end
+   -- end)
 
    local resources = {
       {"CopperOre", 1.0},
@@ -71,14 +74,19 @@ function vanilla_mod.init()
 
    local ss = StaticStructure.new("StartPlatform")
    ss.generate = function(_) print("11111111111") end
-   ss.size = Vec2i.new(10, 10);
-   db:reg(ss)
+   ss.size = Vec2i.new(10, 10)
 
-
-   local a = StaticProp.find("Pine")
-   a.on_destroy = function (pos, attach)
-       print("66666666666666666666666666666666")
-   end
+   local sulphur = StaticProp.find("SulfurCluster")
+   es:sub(defines.events.on_entity_died, function(context)
+      if context.prop == sulphur then
+         local spos = RegionMap.world_block_to_grid(context.pos)
+         local region = regions:get_region(spos)
+         local sd = SourceData.new(region, "test1")
+         sd.item = StaticItem.find("Sulphur")
+         sd.offset = Vec2i.new(context.pos.x, context.pos.y)
+         region:add_source(sd)
+      end
+   end)
 end
 
 function vanilla_mod.post_init()
