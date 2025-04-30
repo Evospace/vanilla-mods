0ff67ab7dd65f532863751323e418cf4004dc034
wavelet-noise <ishellstrike@gmail.com>
2025-04-30 14:04:27 +0200

    fract column wip

---
+++ Vanilla/Blocks/fractionating_column.lua
@@ -0,0 +1,32 @@
+local logic = function(self)
+    local crafter = AutoCrafter.cast(self)
+    crafter.recipes = RecipeDictionary.find("FractionatingColumnRecipeDictionary")
+    crafter.speed = Vlib.get_speed(crafter)
+    crafter.ignore_extra_slots = true
+
+    local inv = ResourceInventory.new(crafter, "rii")
+    inv.item = StaticItem.find("LV")
+    inv.capacity = Vlib.get_consumption(crafter, 20)
+    crafter.energy_input_inventory = inv
+    
+    local acc = ResourceAccessor.new(crafter, "rai")
+    acc.side, acc.pos = Vec3i.back, Vec3i.back
+    acc.inventory = inv
+    acc.is_input = true
+    acc.cover = StaticCover.find("ElectricityInput")
+    acc.channel = "LV"
+
+    local acc = ResourceAccessor.new(crafter, "rai_")
+    acc.side, acc.pos = Vec3i.left, Vec3i.left
+    acc.is_input = true
+    acc.channel = "Fluid"
+    acc.cover = StaticCover.find("FluidInput")
+
+    local acc = ResourceAccessor.new(crafter, "rao")
+    acc.side, acc.pos = Vec3i.front, Vec3i.front + Vec3i.up
+    acc.is_output = true
+    acc.channel = "Fluid"
+    acc.cover = StaticCover.find("FluidOutput")
+end
+
+return { logic_init = logic }
