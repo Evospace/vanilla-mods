d6a4e3fd95e5a0a41e184dfed70516b7268a6174
wavelet-noise <2902221+wavelet-noise@users.noreply.github.com>
2025-07-13 16:27:25 +0200

    Update riteg.lua

--- Vanilla/Blocks/riteg.lua
+++ Vanilla/Blocks/riteg.lua
@@ -17,8 +17,8 @@ local logic = function(self)
     acc.channel = "Heat"
     acc.cover = StaticCover.find("HeatOutput")
 
-    local acc = ResourceAccessor.new(crafter, "rao")
-    acc.side, acc.pos = Vec3i.up, Vec3i.new(0,0,2)
+    local acc = ResourceAccessor.new(crafter, "rao_")
+    acc.side, acc.pos = Vec3i.up, Vec3i.new(0,0,1)
     acc.inventory = inv
     acc.is_output = true
     acc.channel = "Heat"
