141cb9701ff0034931b67a266e1ef59bedf015c3
wavelet-noise <ishellstrike@gmail.com>
2025-07-13 16:22:47 +0200

    RC2

--- Vanilla/Blocks/fractionating_column.lua
+++ Vanilla/Blocks/fractionating_column.lua
@@ -1,10 +1,9 @@
-local energy = 20
+local energy = 200
 
 local logic = function(self)
-    local crafter = AutoCrafter.cast(self)
+    local crafter = AbstractCrafter.cast(self)
     crafter.recipes = RecipeDictionary.find("FractionatingColumnRecipeDictionary")
     crafter.speed = Vlib.get_speed(crafter)
-    crafter.ignore_extra_slots = true
 
     local inv = ResourceInventory.new(crafter, "rii")
     inv.item = StaticItem.find("Electricity")
@@ -19,16 +18,34 @@ local logic = function(self)
     acc.channel = "Electricity"
 
     local acc = ResourceAccessor.new(crafter, "rai_")
-    acc.side, acc.pos = Vec3i.left, Vec3i.left
+    acc.side, acc.pos = Vec3i.front, Vec3i.front + Vec3i.up
     acc.is_input = true
     acc.channel = "Fluid"
     acc.cover = StaticCover.find("FluidInput")
 
     local acc = ResourceAccessor.new(crafter, "rao")
-    acc.side, acc.pos = Vec3i.front, Vec3i.front + Vec3i.up
+    acc.side, acc.pos = Vec3i.left, Vec3i.new(0,1,2)
     acc.is_output = true
     acc.channel = "Fluid"
-    acc.cover = StaticCover.find("FluidOutput")
+    acc.cover = StaticCover.find("FluidOutput1")
+
+    local acc = ResourceAccessor.new(crafter, "rao_")
+    acc.side, acc.pos = Vec3i.back, Vec3i.new(-1,0,3)
+    acc.is_output = true
+    acc.channel = "Fluid"
+    acc.cover = StaticCover.find("FluidOutput2")
+
+    local acc = ResourceAccessor.new(crafter, "rao_1")
+    acc.side, acc.pos = Vec3i.right, Vec3i.new(0,-1,4)
+    acc.is_output = true
+    acc.channel = "Fluid"
+    acc.cover = StaticCover.find("FluidOutput3")
+
+    local acc = ResourceAccessor.new(crafter, "rao_2")
+    acc.side, acc.pos = Vec3i.front, Vec3i.new(1,0,5)
+    acc.is_output = true
+    acc.channel = "Fluid"
+    acc.cover = StaticCover.find("FluidOutput4")
 end
 
 return function(name, tier, level)
