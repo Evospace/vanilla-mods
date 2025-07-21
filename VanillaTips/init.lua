f6ea9a6e19ba2cc8635fda287f42245b865a992c
wavelet-noise <2902221+wavelet-noise@users.noreply.github.com>
2025-07-22 00:16:14 +0200

    more tips

--- VanillaTips/init.lua
+++ VanillaTips/init.lua
@@ -1,6 +1,35 @@
 local VanillaTipsMod = {}
 
 function VanillaTipsMod.init()
+    local i_hud = AutosizeInventory.new_simple()
+    i_hud:add(StaticItem.find("Multitool"), 1)
+    i_hud:add(StaticItem.find("Screwdriver"), 1)
+    i_hud:add(StaticItem.find("StoneFurnace"), 1)
+    db:from_table({
+        class = "StaticTip",
+        name = "Interface",
+        label = Loc.new("Interface", "tips"),
+        description_parts = {
+            Loc.new("InterfaceDescription1", "tips"),
+            Loc.new("InterfaceDescription2", "tips"),
+            Loc.new("InterfaceDescription3", "tips"),
+            Loc.new("InterfaceDescription4", "tips"),
+            Loc.new("InterfaceDescription5", "tips"),
+            Loc.new("InterfaceDescription6", "tips"),
+            Loc.new("InterfaceDescription7", "tips")
+        },
+        image = "Textures/Hud.png",
+        context = i_hud
+    })
+
+    db:from_table({
+        class = "StaticTip",
+        name = "Map",
+        label = Loc.new("Map", "tips"),
+        description_parts = {Loc.new("MapDescription", "tips")},
+        image = "Textures/Map.png"
+    })
+
     local i_first = AutosizeInventory.new_simple()
     i_first:add(StaticItem.find("ChalcopyriteOre"), 1)
     i_first:add(StaticItem.find("MalachiteOre"), 1)
@@ -14,9 +43,17 @@ function VanillaTipsMod.init()
         class = "StaticTip",
         name = "FirstSteps",
         label = Loc.new("FirstSteps", "tips"),
-        description = Loc.new("FirstStepsDescription", "tips"),
+        description_parts = {Loc.new("FirstStepsDescription", "tips")},
         image = "Textures/FirstSteps.png",
         context = i_first
+    })
+
+    db:from_table({
+        class = "StaticTip",
+        name = "Research",
+        label = Loc.new("Research", "tips"),
+        description_parts = {Loc.new("ResearchDescription", "tips")},
+        image = "Textures/Research.png"
     })
 
     local i_acc = AutosizeInventory.new_simple()
@@ -31,7 +68,7 @@ function VanillaTipsMod.init()
         class = "StaticTip",
         name = "Accessors1",
         label = Loc.new("Accessors1", "tips"),
-        description = Loc.new("Accessors1Description", "tips"),
+        description_parts = {Loc.new("Accessors1Description", "tips")},
         image = "Textures/Accessors1.png",
         context = i_acc
     })
@@ -42,7 +79,7 @@ function VanillaTipsMod.init()
         class = "StaticTip",
         name = "Screwdriver",
         label = Loc.new("Screwdriver", "tips"),
-        description = Loc.new("ScrewdriverDescription", "tips"),
+        description_parts = {Loc.new("ScrewdriverDescription", "tips")},
         image = "Textures/Screwdriver.png",
         context = i_scr
     })
@@ -51,7 +88,7 @@ function VanillaTipsMod.init()
         class = "StaticTip",
         name = "RotationWhileBuilding",
         label = Loc.new("RotationWhileBuilding", "tips"),
-        description = Loc.new("RotationWhileBuildingDescription", "tips"),
+        description_parts = Loc.new("RotationWhileBuildingDescription", "tips"),
         image = "Textures/Rotation.png"
     })
 
@@ -59,7 +96,7 @@ function VanillaTipsMod.init()
         class = "StaticTip",
         name = "BlockPipette",
         label = Loc.new("BlockPipette", "tips"),
-        description = Loc.new("BlockPipetteDescription", "tips"),
+        description_parts = {Loc.new("BlockPipetteDescription", "tips")},
         image = "Textures/Pipette.png"
     })
 
@@ -67,7 +104,7 @@ function VanillaTipsMod.init()
         class = "StaticTip",
         name = "StackTransfers",
         label = Loc.new("StackTransfers", "tips"),
-        description = Loc.new("StackTransfersDescription", "tips"),
+        description_parts = {Loc.new("StackTransfersDescription", "tips")},
         image = "Textures/StackTransfers.png"
     })
 end
