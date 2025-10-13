8bcdb666597ef2efffb2bae2c0c959e2f0d241a4
wavelet-noise <2902221+wavelet-noise@users.noreply.github.com>
2025-10-13 21:41:54 +0300

    Update audio.lua

--- Core/audio.lua
+++ Core/audio.lua
@@ -1,12 +1,12 @@
 return function()
-   local function make_audio(name, class) 
+   local function make_audio(name, class, default) 
       db:from_table({
          class = "Setting",
          category = "Audio",
          type = "Slider",
          max_value = 100,
          min_value = 0,
-         int_default_value = 100,
+         int_default_value = default,
          ---@param setting Setting
          set_action = function(setting)
             local value = setting.int_value / 100.0
@@ -20,11 +20,11 @@ return function()
       })
    end
 
-   make_audio("Master", "EvospaceMaster")
-   make_audio("Music", "Music")
-   make_audio("Blocks", "Blocks")
-   make_audio("Ambient", "Ambient")
-   make_audio("NatureAmbient", "NatureAmbient")
-   make_audio("Footsteps", "Footsteps")
-   make_audio("UI", "UI")
+   make_audio("Master", "EvospaceMaster", 100)
+   make_audio("Music", "Music", 60)
+   make_audio("Blocks", "Blocks", 100)
+   make_audio("Ambient", "Ambient", 100)
+   make_audio("NatureAmbient", "NatureAmbient", 100)
+   make_audio("Footsteps", "Footsteps", 100)
+   make_audio("UI", "UI", 100)
  end
