1f7775721072cbd59ca5fb5f530ae3c0fb8683d0
wavelet-noise <2902221+wavelet-noise@users.noreply.github.com>
2025-12-30 03:59:52 +0300

    Create heat_pipe.lua

---
+++ Vanilla/Blocks/heat_pipe.lua
@@ -0,0 +1,25 @@
+local logic = function(self)
+    local conductor = HeatConductorBlockLogic.cast(self)
+
+    local sides = {
+        Vec3i.back, Vec3i.front, Vec3i.right, Vec3i.left, Vec3i.down, Vec3i.up
+    }
+
+    local level = conductor.static_block.level
+
+    for index, side in pairs(sides) do
+        local acc = ResourceAccessor.new(conductor, "Heat"..index)
+        acc.side, acc.pos = side, Vec3i.zero
+        acc.channel = "Heat"
+        conductor:add_wire(acc)
+    end
+
+    conductor.capacity = 32000
+    conductor.conductor_channel = 3000
+    conductor.channel = "Heat"
+end
+
+return function(name, tier, level)
+    LocData.set(name, Vlib.ToPower(level, 1))
+    return { logic_init = logic }
+end
