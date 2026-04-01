f778a3cdf3d2632f155ce84513208ccd3449382d
wavelet-noise <ishellstrike@gmail.com>
2026-04-01 22:06:28 +0300

    farm rollback to basic crafter

--- Vanilla/Blocks/automatic_farm.lua
+++ Vanilla/Blocks/automatic_farm.lua
@@ -1,33 +1,16 @@
 local logic = function(self)
-    local crafter = DrillingMachineBase.cast(self)
-    crafter.map_register = true
-
-    crafter.energy_per_tick = 3 * (crafter.static_block.level + 1)
-    crafter.productivity = 25 * crafter.static_block.level
-    crafter.ticks_per_item = 1200
-
-    local inv = ResourceInventory.new(crafter, "energy")
-    inv.capacity = crafter.energy_per_tick
-    inv.item = StaticItem.find("Water")
-    crafter.energy = inv
+    local crafter = AbstractCrafter.cast(self)
+    crafter.recipes = RecipeDictionary.find("AutomaticFarmRecipeDictionary")
+    crafter.speed = Vlib.get_speed(crafter)
+    -- crafter.map_register = true
 
     local acc = ResourceAccessor.new(crafter, "rai")
     acc.side, acc.pos = Vec3i.front, Vec3i.zero
     acc.is_input = true
-    acc.inventory = inv
     acc.channel = "Fluid"
-
-    local invs = Vlib.add_single_slot_invs(crafter.inventory, crafter, "ii", 1)
-    invs[1].capacity = 100
-
-    local acc = BaseInventoryAccessor.new(crafter, "oa")
-    acc.side, acc.pos = Vec3i.back, Vec3i.new(0, -1, 0)
-    acc.output = invs[1]
-    acc.auto_output = true
-    acc.cover = StaticCover.find("ItemOutput")
+    acc.cover = StaticCover.find("FluidInput")
 end
 
 return function(name, tier, level)
-    LocData.set(name, Vlib.ToPower(level, 1))
     return { logic_init = logic }
 end
